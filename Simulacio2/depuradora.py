53bd3519c27f139f819fc1efdd04b0797168d747
lluisroyo <40797783+lluisroyo@users.noreply.github.com>
2022-01-15 11:40:25 +0100

    Add files via upload

--- Simulacio2/depuradora.py
+++ Simulacio2/depuradora.py
@@ -3,7 +3,7 @@ import numpy as np
 dl = 0.001
 r = 0.25
 N = 1001
-v = 0.5
+k = 0.1
 
 rho_old = np.zeros((N,N))
 rho_new = np.zeros((N,N))
@@ -16,24 +16,24 @@ for i in range(401,600,1):
 for j in range(801,1000,1):
     rho_old[j][0] = 1.5 #rho2
 
-# Definim la densitat que extra la depuradora
+# Definim la densitat que extrau la depuradora
 
 for i in range(401,600,1):
     for j in range(801,1000,1):
-        S[j][i] = -0.5
+        S[j][i] = -1
 
 #comencem el bucle temporal explicit
 
-for n in range (1,50,1):
+for n in range (1,10,1):
 
     for j in range(1,801,1):  #metode numeric per al flux vertical (J_y)
         for i in range(401,600,1):
-            rho_new[j][i] = rho_old[j][i] - (r/2.0) * v *(rho_old[j][i+1]+rho_old[j+1][i]-rho_old[j][i-1]-rho_old[j-1][i] ) + ( ((r**2*v**2)/2.0) + (r / dl)) * (rho_old[j][i+1]+rho_old[j][i-1] + rho_old[j+1][i] + rho_old[j-1][i] - 4.0*rho_old[j][i]) 
-            if rho_new[j][i]<0.0:
+            rho_new[j][i] = (1.0/4.0 - r/2.0 ) * (rho_old[j][i+1]-rho_old[j][i-1] + rho_old[j+1][i] - rho_old[j-1][i]) + (k*r/ dl) *(rho_old[j][i+1]+rho_old[j][i-1] + rho_old[j-1][i] + rho_old[j+1][i] -4.0*rho_old[j][i])
+            if rho_new[j][i]<0.0: #comprovem que cap punt tingui densitat negativa
                 rho_new[j][i] = 0.0
     for j in range(801,1000,1):
         for i in range(1,1000,1):
-            rho_new[j][i] = rho_old[j][i] - (r/2.0) * v *(rho_old[j][i+1]+rho_old[j+1][i]-rho_old[j][i-1]-rho_old[j-1][i] ) + ( ((r**2*v**2)/2.0) + (r / dl)) * (rho_old[j][i+1]+rho_old[j][i-1] + rho_old[j+1][i] + rho_old[j-1][i] - 4.0*rho_old[j][i]) + S[j][i]
+            rho_new[j][i] = (1.0/4.0 - r/2.0 ) * (rho_old[j][i+1]-rho_old[j][i-1] + rho_old[j+1][i] - rho_old[j-1][i]) + (k*r/ dl) *(rho_old[j][i+1]+rho_old[j][i-1] + rho_old[j-1][i] + rho_old[j+1][i] -4.0*rho_old[j][i]) + S[j][i]
             if rho_new[j][i]<0.0:
                 rho_new[j][i] = 0.0
 
